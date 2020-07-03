3b3f008061e0bb753c679465e4d1999d72bbdf69
SeongGyeong <deborahoh94@gmail.com>
2020-07-03 14:53:28 +0900

    commit

--- app/src/main/java/com/osg/ex93githubtest/MainActivity.java
+++ app/src/main/java/com/osg/ex93githubtest/MainActivity.java
@@ -7,15 +7,17 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void    onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         int a=10;
-        
+
         int b=1000;
         String s = "Hello GitHub";
 
         //코드 또 바꿔서 깃헙 dev와 연결하기
+
+        //check
     }
 }
